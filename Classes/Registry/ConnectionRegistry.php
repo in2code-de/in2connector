8be03168205120f33e081ea76c1bac4ea53f4b41
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-01-04 14:51:29 +0100

    [TASK] Add logging to ConnectionRegistry and method to retrieve a formerly required connection

--- Classes/Registry/ConnectionRegistry.php
+++ Classes/Registry/ConnectionRegistry.php
@@ -1,15 +1,22 @@
 <?php
 namespace In2code\In2connector\Registry;
 
+use In2code\In2connector\Domain\Factory\RepositoryFactory;
 use In2code\In2connector\Domain\Model\AbstractConnection;
+use In2code\In2connector\Domain\Repository\AbstractConnectionRepository;
 use In2code\In2connector\Exceptions\ConnectionTypeNotSupportedException;
+use In2code\In2connector\Logging\LoggerTrait;
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 /**
  * Class ConnectionRegistry
  */
 class ConnectionRegistry implements SingletonInterface
 {
+    use LoggerTrait;
+
     /**
      * @var AbstractConnection[][]
      */
@@ -23,11 +30,40 @@ class ConnectionRegistry implements SingletonInterface
      */
     public function requireConnection($package, $identityKey, $type)
     {
-        if (is_subclass_of($type, AbstractConnection::class)) {
+        $this->getLogger()->debug(
+            'Requiring connection',
+            [
+                'package' => $package,
+                'identityKey' => $identityKey,
+                'type' => $type,
+            ]
+        );
+        if (is_subclass_of(
+            $type,
+            AbstractConnection::class
+        )) {
             $this->requiredConnections[$package][$identityKey] = $type;
+
+            $this->getLogger()->debug(
+                'Requiring connection successful',
+                [
+                    'package' => $package,
+                    'identityKey' => $identityKey,
+                    'type' => $type,
+                ]
+            );
         } else {
+            $this->getLogger()->critical(
+                'Requiring connection failed: "The connection class is not supported"',
+                [
+                    'package' => $package,
+                    'identityKey' => $identityKey,
+                    'type' => $type,
+                ]
+            );
+
             throw new ConnectionTypeNotSupportedException(
-                'The connection class ' . htmlspecialchars($type) . ' is not supported',
+                'The connection class ' . $type . ' is not supported',
                 1450371768
             );
         }
@@ -39,5 +75,76 @@ class ConnectionRegistry implements SingletonInterface
     public function getRequiredConnections()
     {
         return $this->requiredConnections;
+    }
+
+    /**
+     * @param string $package
+     * @param string $identityKey
+     * @return AbstractConnection|null
+     */
+    public function getRequiredConnection($package, $identityKey)
+    {
+        $this->getLogger()->debug(
+            'Finding requested connection',
+            [
+                'package' => $package,
+                'identityKey' => $identityKey,
+            ]
+        );
+        if (!empty($this->requiredConnections[$package][$identityKey])) {
+            $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+            $repositoryFactory = $objectManager->get(RepositoryFactory::class);
+
+            $repository = $repositoryFactory->getRepositoryForClassName(
+                $this->requiredConnections[$package][$identityKey]
+            );
+
+            if ($repository instanceof AbstractConnectionRepository) {
+                $connection = $repository->findOneByPackageAndIdentityKey(
+                    $package,
+                    $identityKey
+                );
+
+                if ($connection instanceof AbstractConnection) {
+                    $this->getLogger()->info(
+                        'Found requested connection',
+                        [
+                            'package' => $package,
+                            'identityKey' => $identityKey,
+                            'type' => $this->requiredConnections[$package][$identityKey],
+                        ]
+                    );
+                    return $connection;
+                } else {
+                    $this->getLogger()->error(
+                        'The required connection does not exist',
+                        [
+                            'package' => $package,
+                            'identityKey' => $identityKey,
+                            'type' => $this->requiredConnections[$package][$identityKey],
+                        ]
+                    );
+                }
+            } else {
+                $this->getLogger()->error(
+                    'The repository for the required connection could not be found',
+                    [
+                        'package' => $package,
+                        'identityKey' => $identityKey,
+                        'type' => $this->requiredConnections[$package][$identityKey],
+                    ]
+                );
+            }
+        } else {
+            $this->getLogger()->error(
+                'The searched connection was not required, thus there is no type',
+                [
+                    'package' => $package,
+                    'identityKey' => $identityKey,
+                ]
+            );
+        }
+        return null;
     }
 }
