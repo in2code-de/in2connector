2fc77bdb1f6f69be080a270f39d8f807ece6c0b3
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-01-11 11:27:56 +0100

    [TASK] Add missing copyright and licence notes

--- Classes/Controller/DashboardController.php
+++ Classes/Controller/DashboardController.php
@@ -1,6 +1,23 @@
 <?php
 namespace In2code\In2connector\Controller;
 
+/*
+ * Copyright notice
+ *
+ * (c) 2015 Oliver Eglseder <oliver.eglseder@in2code.de>, in2code GmbH
+ *
+ * All rights reserved
+ *
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 use In2code\In2connector\Domain\Model\Cato\ConnectionLinker;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
--- Classes/Registry/ConnectionRegistry.php
+++ Classes/Registry/ConnectionRegistry.php
@@ -1,6 +1,25 @@
 <?php
 namespace In2code\In2connector\Registry;
 
+/*
+ * Copyright notice
+ *
+ * (c) 2015 Oliver Eglseder <oliver.eglseder@in2code.de>, in2code GmbH
+ *
+ * All rights reserved
+ *
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use In2code\In2connector\Domain\Model\Dto\ConnectionDemand;
 use In2code\In2connector\Domain\Model\Dto\DriverRegistration;
 use In2code\In2connector\Driver\AbstractDriver;
