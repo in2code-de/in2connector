6b5975a0fed15e6abd7d2ee253230fe93623b87c
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-01-29 14:55:57 +0100

    [TASK] Remove not-up-to-date test

--- Tests/Unit/Registry/Fixtures/InvalidDriver.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace In2code\In2connector\Tests\Unit\Registry\Fixtures;
-
-class InvalidDriver
-{
-}
--- Tests/Unit/Registry/Fixtures/ValidDriver.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace In2code\In2connector\Tests\Unit\Registry\Fixtures;
-
-use In2code\In2connector\Driver\AbstractDriver;
-
-class ValidDriver extends AbstractDriver
-{
-}
